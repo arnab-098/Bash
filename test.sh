d0ddc7f91e982e357cde36c0b313d508fa564e9f
arnab <arnab.chattopadhyay777@gmail.com>
2024-08-28 15:51:40 +0530

    practice

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
-read -p "Enter a number: " num
+file="./data.txt"
 
-while [ $num -gt 0 ]; do
-  echo $num
-  num=$(expr $num - 1)
+n=$(expr $(cat $file | wc -l) - 1)
+names1=$(cat $file | tail -n $n | cut -d "|" -f 2 | sort | uniq -d)
+names2=$(cat $file | tail -n $n | cut -d "|" -f 2 | sort | uniq -u)
+
+names="$names1 $names2"
+
+for name in $names; do
+  echo $name
 done
-
-echo "Done"
