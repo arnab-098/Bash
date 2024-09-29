323d31610c42474c9ee77a129eb53b3ff3edea3d
arnab <arnab.chattopadhyay777@gmail.com>
2024-09-29 12:14:56 +0530

    ipsweep

---
+++ scan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "Invalid Syntax"
+  echo "Syntax: ./scan.sh <ip>"
+else
+  for ip in $(seq 1 255); do
+    ping $1.$ip -c 1 | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+  done
+fi
