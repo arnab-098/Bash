788764a068337182dd3ad44dc1b6b56f3f0c479a
arnab-098 <arnab.chattopadhyay777@gmail.com>
2024-10-20 13:12:30 +0530

    gave permissions

--- processKiller.sh
+++ processKiller.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-	echo "Invalid Syntex";
-	echo "Syntax: ./processKiller.sh <process name>"
-	exit 1
+  echo "Invalid Syntex"
+  echo "Syntax: ./processKiller.sh <process name>"
+  exit 1
 fi
 
 for process in "$@"; do
-	pids=$(ps -eaf | tr -s " " | grep -iP "^([^ ]* ){7}$process$" | cut -d " " -f 2)
-	
-	if [ "$pids" == "" ]; then
-		echo "No such process: $process"
-	else
-		kill $pids
-		wait $pids 2>/dev/null
-		echo "Killed process(s): $process"
-	fi
-done
+  pids=$(ps -eaf | tr -s " " | grep -iP "^([^ ]* ){7}$process$" | cut -d " " -f 2)
 
+  if [ "$pids" == "" ]; then
+    echo "No such process: $process"
+  else
+    kill $pids
+    wait $pids 2>/dev/null
+    if [ -z "$(ps -eaf | tr -s " " | grep -iP "^([^ ]* ){7}$process$")" ]; then
+      echo "Killed process(s): $process"
+    else
+      echo "Failed to kill process: $process"
+    fi
+  fi
+done
