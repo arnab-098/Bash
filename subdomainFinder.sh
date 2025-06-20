b480fa7c72cea9afdc3dd774c9828ca74ea327a6
arnab-098 <arnab.chattopadhyay777@gmail.com>
2025-06-20 13:18:35 +0530

    added httprobe

--- subdomainFinder.sh
+++ subdomainFinder.sh
@@ -21,7 +21,11 @@ assetfinder $url >>$HOME/$url/recon/assets.txt
 cat $url/recon/assets.txt | grep "$1" >>$HOME/$url/recon/final.txt
 rm $HOME/$url/recon/assets.txt
 
-echo "[+] Harvesting subdomains with amass..."
-amass $url >>$HOME/$url/recon/f.txt
-sort -u $HOME/$url/recon/f.txt >>$HOME/$url/final.txt
-rm $HOME/$url/recon/f.txt
+#echo "[+] Harvesting subdomains with amass..."
+#amass $url >>$HOME/$url/recon/f.txt
+#sort -u $HOME/$url/recon/f.txt >>$HOME/$url/final.txt
+#rm $HOME/$url/recon/f.txt
+
+echo "[+] Probing for alive domains..."
+cat $HOME/$url/recon/final.txt | sort -u | httprobe -s -p https:443 | sed 's|^https://||' | tr -d ':443' >>$HOME/$url/recon/alive.txt
+rm $HOME/$url/recon/final.txt
