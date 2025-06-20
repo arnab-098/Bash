6f13ff0f146456a246acc4fccabb87e34017d499
arnab-098 <arnab.chattopadhyay777@gmail.com>
2025-06-20 12:36:49 +0530

    assetfinder with grep

---
+++ runAssetFinder.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Invalid syntax"
+    echo "$(basename "$0") <url>"
+    exit
+fi
+
+url=$1
+
+if [ ! -d "$url" ]; then
+    mkdir $HOME/$url
+fi
+
+if [ ! -d "$url/recon" ]; then
+    mkdir $HOME/$url/recon
+fi
+
+echo "[+] Harvesting subdomains with assetfinder..."
+
+assetfinder $url >>$HOME/$url/recon/assets.txt
+
+cat $url/recon/assets.txt | grep "$1" >>$HOME/$url/recon/final.txt
+
+rm $HOME/$url/recon/assets.txt
