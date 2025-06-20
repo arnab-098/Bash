79b6709bb07f6e3c5958c4e477e0f1f81e4b118c
arnab-098 <arnab.chattopadhyay777@gmail.com>
2025-06-20 12:52:58 +0530

    added amass

--- subdomainFinder.sh
+++ subdomainFinder.sh
@@ -17,9 +17,11 @@ if [ ! -d "$url/recon" ]; then
 fi
 
 echo "[+] Harvesting subdomains with assetfinder..."
-
 assetfinder $url >>$HOME/$url/recon/assets.txt
-
 cat $url/recon/assets.txt | grep "$1" >>$HOME/$url/recon/final.txt
-
 rm $HOME/$url/recon/assets.txt
+
+echo "[+] Harvesting subdomains with amass..."
+amass $url >>$HOME/$url/recon/f.txt
+sort -u $HOME/$url/recon/f.txt >>$HOME/$url/final.txt
+rm $HOME/$url/recon/f.txt
